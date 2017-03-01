e1e3edf916f1767baaf76334075e69cb3e0732fe
rose kariuki <rosekariuki28@gmail.com>
2017-03-01 07:57:53 +0000

    rose question_2

--- quiz-questions/question_2.py
+++ quiz-questions/question_2.py
@@ -29,7 +29,7 @@ def number_of_customers_per_state(customers):
     }
     """
     # Write your code here
-    pass
+    return {key: len(value) for key, value in customers.iteritems()}
 
 
 class NumberOfCustomersPerStateTestCase(unittest.TestCase):
